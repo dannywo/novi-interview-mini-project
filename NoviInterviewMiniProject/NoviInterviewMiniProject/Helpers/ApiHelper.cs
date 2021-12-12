dab3875461fccd03fcd0f6e17326c9c6090fb171
Danny O <dannywo@gmail.com>
2021-12-12 13:50:05 -0800

    Adds Helpers folder and ApiHelper class

---
+++ NoviInterviewMiniProject/NoviInterviewMiniProject/Helpers/ApiHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace NoviInterviewMiniProject.Helpers
+{
+    public class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; } = new HttpClient();
+
+        public static void Initialize()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
