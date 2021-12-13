419434c0da37b341faf494cfd58da791b3597600
Danny O <dannywo@gmail.com>
2021-12-13 15:03:41 -0800

    Adds missing model class Membertype

---
+++ NoviInterviewMiniProject/NoviInterviewMiniProject/Models/Membertype.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NoviInterviewMiniProject.Models
+{
+    public class Membertype
+    {
+        public string UniqueID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool ForCompanies { get; set; }
+    }
+}
