772960397450056a2cc8c7a3453c628a501f7162
Danny O <dannywo@gmail.com>
2021-12-13 21:34:06 -0800

    Assigns webconfig keys to the url and token properties.

--- NoviInterviewMiniProject/NoviInterviewMiniProject/Controllers/ThemeParkController.cs
+++ NoviInterviewMiniProject/NoviInterviewMiniProject/Controllers/ThemeParkController.cs
@@ -7,25 +7,27 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
 
 namespace NoviInterviewMiniProject.Controllers
 {
     public class ThemeParkController : Controller
     {
-        public string _url { get; set; } = "https://ftpa.novistaging.com/api/members";
-        private string _token { get; } = "";
+        private string _url { get; set; } = ConfigurationManager.AppSettings["ApiUrl"];
+        private string _token { get; } = ConfigurationManager.AppSettings["ApiKey"];
         private List<MemberResult> members { get; set; } = new List<MemberResult>();
 
-        // GET: ThemePark
-        public async Task<ActionResult> Index()
+        // GET: Theme Park Members
+        public async Task<ActionResult> Members()
         {
+            var section = "/members";
             members = TempData["Members"] as List<MemberResult>;
             if (members == null || members.Count == 0)
             {
                 ApiHelper.Initialize();
                 ApiHelper.ApiClient.DefaultRequestHeaders.Add("Authorization", $"Basic {_token}");
 
-                using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(_url))
+                using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync($"{_url}{section}"))
                 {
                     if (response.IsSuccessStatusCode)
                     {
@@ -33,7 +35,7 @@ namespace NoviInterviewMiniProject.Controllers
                         members = memberResponse.Results.Where(item => item.Active == true).ToList();
                         TempData["Members"] = members;
                         return View(members);
-                    }
+                    }   
                     else
                     {
                         return View(response.ReasonPhrase);
